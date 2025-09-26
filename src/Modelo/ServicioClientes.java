ce9bc5b9b8b1dbde3385a4938bb5a6ab7f0d5cb7
eduardsalas <edusalasmurqest.utn.ac.cr>
2025-09-25 21:16:24 -0600

    Servicio Cliente
    Se creo una funcion formatoNombrePropiopara guardar el nombre del usuario con la primera letra en mayuscula

--- src/Modelo/ServicioClientes.java
+++ src/Modelo/ServicioClientes.java
@@ -26,13 +26,14 @@ public class ServicioClientes {
         validarRequeridos(id, nombre, correo, telefono);
         if (gestor.existe(id)) throw new IllegalArgumentException("Ya existe un registro con id=" + id);
         if (!EMAIL.matcher(correo).matches()) throw new IllegalArgumentException("Formato de correo inválido");
+        String nombreFormateado = formatoNombrePropio(nombre);
         gestor.guardar(new Cliente(id, nombre, correo, telefono, preferente));
     }
     
     public void actualizar(String id, String correo, String telefono, boolean preferente) {
         Objects.requireNonNull(ultimoRegistro(), "No se ha cargado ningun registro");
         validarRequeridos(id, correo, telefono);
-        if(!hayCambios(id, correo, telefono)) return;
+        if(!hayCambios(id, correo, telefono,preferente)) return;
         if (!gestor.existe(id)) throw new IllegalArgumentException("No existe un registro con id=" + id);
         if (!EMAIL.matcher(correo).matches()) throw new IllegalArgumentException("Formato de correo inválido");
         Cliente cliente=gestor.buscar(id);
@@ -75,10 +76,22 @@ public class ServicioClientes {
         }
     }
     
-    private boolean hayCambios(String id,String correo,String telefono){
+    private String formatoNombrePropio(String nombre) {
+        if (nombre == null || nombre.isBlank()) return nombre;
+        String[] palabras = nombre.toLowerCase().trim().split("\\s+");
+        StringBuilder nombreFormateado = new StringBuilder();
+        for (String palabra : palabras) {
+            if (palabra.length() > 0) {
+                nombreFormateado.append(Character.toUpperCase(palabra.charAt(0))).append(palabra.substring(1)).append(" ");
+            }
+        }
+        return nombreFormateado.toString().trim();
+    }
+    
+    private boolean hayCambios(String id,String correo,String telefono,boolean preferente){
         Cliente cliente = gestor.buscar(id);
         Objects.requireNonNull(cliente, "No se ha cargado ningun registro");
         validarRequeridos(correo,telefono);
-        return !(cliente.getCorreo().equals(correo) && cliente.getTelefono().equals(telefono));
+        return !(cliente.getCorreo().equals(correo) && cliente.getTelefono().equals(telefono)&& cliente.getPreferente()==preferente);
     }
 }
