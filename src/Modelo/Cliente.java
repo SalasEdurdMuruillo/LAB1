40bd23fffb37dd483c422475ac6932bd771744a1
eduardsalas <edusalasmurqest.utn.ac.cr>
2025-09-25 20:34:18 -0600

    Modificacion Clase Cliente
    Se agrego el atributo preferente en la clase Cliente

--- src/Modelo/Cliente.java
+++ src/Modelo/Cliente.java
@@ -15,19 +15,23 @@ public class Cliente {
     private final String nombre;
     private String correo;
     private String telefono;
+    private boolean preferente; 
     
     public String getId() { return id; }
     public String getNombre() { return nombre; }
     public String getCorreo() { return correo; }
     public String getTelefono() { return telefono; }
+    public boolean getPreferente(){return preferente;}
     
     public void setCorreo(String correo) { this.correo = Objects.requireNonNull(correo,"El correo no puede ser un valor nulo"); }
     public void setTelefono(String telefono) { this.telefono = Objects.requireNonNull(telefono,"El telefono no puede ser un valor nulo"); }
-
-    public Cliente(String id, String nombre, String correo, String telefono) {
+    public void setPreferente(boolean preferente){this.preferente = preferente;}
+    
+    public Cliente(String id, String nombre, String correo, String telefono,boolean preferete) {
         this.id = Objects.requireNonNull(id,"El id no puede ser un valor nulo");
         this.nombre = Objects.requireNonNull(nombre,"El nombre no puede ser un valor nulo");
         this.correo = Objects.requireNonNull(correo,"El correo no puede ser un valor nulo");
         this.telefono = Objects.requireNonNull(telefono,"El telefono no puede ser un valor nulo");
+        this.preferente = preferente;
     }
 }
